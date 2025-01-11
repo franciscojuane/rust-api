93acf08e72760eceabd49cbdd287040ebaa81f6b
Francisco Juane <franciscojuaneutn@gmail.com>
2025-01-11 17:01:07 -0300

    Changed numeration to previous migration

--- migration/src/m20220101_000002_create_item_table.rs
+++ migration/src/m20220101_000002_create_item_table.rs
@@ -34,7 +34,7 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
-        todo!();
+        //todo!();
 
         manager
             .drop_table(Table::drop().table(Item::Table).to_owned())
