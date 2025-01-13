589cddae280bf82ee4b60ce93ec9f0c622440447
Francisco Juane <franciscojuaneutn@gmail.com>
2025-01-13 16:16:03 -0300

    Faker library added. Dataloader changed to implement automatically several warehouses and items using faker. Implemented Error for CustomError

--- src/data_loader.rs
+++ src/data_loader.rs
@@ -2,11 +2,17 @@ use std::error::Error;
 use std::ops::Deref;
 use std::sync::Arc;
 use chrono::Utc;
+use fake::{faker, rand, Fake, Rng};
+use fake::faker::address::en::{CityName, StateAbbr};
+use fake::faker::address::raw::StreetName;
+use fake::faker::name::en::Name;
+use fake::faker::number::raw::NumberWithFormat;
+use fake::locales::EN;
 use sea_orm::{ActiveValue, Database, DatabaseConnection, EntityTrait, IntoActiveModel};
 use crate::entities::prelude::Warehouse;
 use crate::entities::prelude::Item;
 use crate::entities::{item, warehouse};
-use crate::entities::warehouse::Column::Region;
+use crate::entities::warehouse::Column::{Region, Street};
 use crate::enums::enums::RegionEnum;
 use crate::models::address::Address;
 use crate::repositories::item::ItemRepository;
@@ -16,7 +22,7 @@ pub struct AppState{
     pub database_connection: DatabaseConnection
 }
 
-pub async fn load_data() -> Result<(), dyn Error>{
+pub async fn load_data() -> Result<(), Box<dyn Error>>{
     let database_url = "mysql://root:root@localhost:3306/warehouse_db";
     let db = Database::connect(database_url).await.unwrap();
 
@@ -27,53 +33,39 @@ pub async fn load_data() -> Result<(), dyn Error>{
     let mut warehouse_repository = WarehouseRepository::new(Arc::clone(&appState));
     let mut item_repository = ItemRepository::new(Arc::clone(&appState));
 
+    let mut warehouse_id = 0;
+    for i in 1..=10 {
+        let name: String = faker::name::en::FirstName().fake();
+        let warehouse1 = warehouse::Model {
+            id: Default::default(),
+            warehouse_key: format!("WAREHOUSE_{}", name.to_uppercase()),
+            name: format!("{} Warehouse", name),
+            street: StreetName(EN).fake(),
+            number: rand::thread_rng().gen_range(1..1000),
+            city: CityName().fake(),
+            region: StateAbbr().fake(),
+            postal_code: NumberWithFormat(EN, "#####").fake(),
+            ..Default::default()
+        };
 
-    let warehouse1 = warehouse::Model {
-        id: Default::default(),
-        warehouse_key: "MAIN_WAREHOUSE".to_string(),
-        name: "Main Warehouse".to_string(),
-        street: "Fake Street".to_string(),
-        number: 123,
-        city: "Miami".to_string(),
-        region: "FL".to_string(),
-        postal_code: "12345".to_string(),
-        ..Default::default()
-    };
+        warehouse_id = warehouse_repository.create(&warehouse1).await?;
 
-    let warehouse1_id = warehouse_repository.create(&warehouse1).await?;
+        let item_list = ["iPhone 14", "iPhone 14 Pro", "iPhone 14 Pro Max", "Samsung Galaxy",
+        "Sony Projector", "Asus Display", "Logitech Camera", "Genius Mouse"];
+        for j in item_list {
+            let item = item::Model{
+                id: Default::default(),
+                name: j.to_owned(),
+                units: rand::thread_rng().gen_range(1..300) ,
+                price: ((rand::thread_rng().gen_range(500.00..1000.00) * 100.00) as f64).round() / 100.00,
+                warehouse_id,
+                ..Default::default()
+            };
+            item_repository.create(&item).await?;
+        }
 
-    let item1 = item::Model{
-        id: Default::default(),
-        name: "iPhone 14 ".to_owned(),
-        units: 23,
-        price: 500.00,
-        warehouse_id: warehouse1_id,
-        ..Default::default()
-    };
 
-    let item1_id = item_repository.create(&item1).await?;
-
-    let item2 = item::Model{
-        id: Default::default(),
-        name: "iPhone 14 Pro ".to_owned(),
-        units: 38,
-        price: 700.00,
-        warehouse_id: warehouse1_id,
-        ..Default::default()
-    };
-
-    let item2_id = item_repository.create(&item2).await?;
-
-    let item3 = item::Model{
-        id: Default::default(),
-        name: "iPhone 14 Pro Max".to_owned(),
-        units: 15,
-        price: 900.00,
-        warehouse_id: warehouse1_id,
-        ..Default::default()
-    };
-
-    let item3_id = item_repository.create(&item3).await?;
+    }
 
     Ok(())
 }
--- src/errors/errors.rs
+++ src/errors/errors.rs
@@ -1,3 +1,7 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+use sea_orm::DeriveDisplay;
+
 #[derive(Debug)]
 pub enum CustomError {
     ElementNotFound,
@@ -5,3 +9,14 @@ pub enum CustomError {
     UpdateError,
     ReadError,
 }
+
+impl Display for CustomError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Custom error occurred: {} " , self)
+    }
+}
+
+impl Error for CustomError {
+
+}
+
